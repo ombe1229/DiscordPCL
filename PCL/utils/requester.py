2505a5720528af20296f8fe385fccb24e0b9fb5e
ombe <h3236516@gmail.com>
2021-07-04 22:31:15 +0900

    refactor(requester): create get, post

--- PCL/utils/requester.py
+++ PCL/utils/requester.py
@@ -12,9 +12,15 @@ class Response:
 
 class Requester:
     __base_url = "https://discord.com/api/v8/"
+    __token = token
 
-    async def get(self, url: str, token: str):
-        header = {"Authorization": token}
-        async with aiohttp.ClientSession(headers=header) as cs:
-            async with cs.get(self.__base_url + url) as r:
+    async def request(self, method: str, url: str, **kwargs: Any):
+        async with aiohttp.ClientSession(headers={"Authorization": self.__token}) as cs:
+            async with cs.request(method, self.__base_url + url, **kwargs) as r:
                 return Response(r.status, await r.json())
+
+    async def get(self, url: str, **kwargs: Any):
+        return await self.request("GET", url, **kwargs)
+
+    async def post(self, url: str, **kwargs: Any):
+        return await self.request("POST", url, **kwargs)
