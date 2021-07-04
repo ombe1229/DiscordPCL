11b13cd6e75167ab7b564d57c558e019895c3cc3
ombe <h3236516@gmail.com>
2021-07-04 23:05:59 +0900

    refactor(requester): change structure

--- PCL/utils/requester.py
+++ PCL/utils/requester.py
@@ -1,7 +1,7 @@
 from typing import Any
 import aiohttp
+from aiohttp.typedefs import StrOrURL
 from attr import dataclass
-from token import token
 
 
 @dataclass
@@ -11,16 +11,17 @@ class Response:
 
 
 class Requester:
-    __base_url = "https://discord.com/api/v9/"
-    __token = token
+    def __init__(self, base_url: StrOrURL, headers):
+        self.base_url = base_url
+        self.headers = headers
 
-    async def request(self, method: str, url: str, **kwargs: Any):
-        async with aiohttp.ClientSession(headers={"Authorization": self.__token}) as cs:
+    async def request(self, method: str, url: StrOrURL, **kwargs: Any):
+        async with aiohttp.ClientSession(headers=self.headers) as cs:
             async with cs.request(method, self.__base_url + url, **kwargs) as r:
                 return Response(r.status, await r.json())
 
-    async def get(self, url: str, **kwargs: Any):
+    async def get(self, url: StrOrURL, **kwargs: Any):
         return await self.request("GET", url, **kwargs)
 
-    async def post(self, url: str, **kwargs: Any):
+    async def post(self, url: StrOrURL, **kwargs: Any):
         return await self.request("POST", url, **kwargs)
