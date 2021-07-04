8fc8a83f295570c25da800227c67e6910bacd831
ombe <h3236516@gmail.com>
2021-07-05 02:04:39 +0900

    feat(discord_client_): add get_guild, get_guild_channels

--- PCL/discord_client.py
+++ PCL/discord_client.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from PCL.utils.requester import Requester
 
 
@@ -9,3 +10,11 @@ class DiscordRequester:
 
     async def get_user_guilds(self):
         return await self.requester.get("users/@me/guilds")
+
+    async def get_guild(self, guild_id: int, with_counts: Optional[bool] = False):
+        return await self.requester.get(
+            f"guilds/{guild_id}", params={"with_counts": f"{with_counts}"}
+        )
+
+    async def get_guild_channels(self, guild_id: int):
+        return await self.requester.get(f"guilds/{guild_id}/channels")
